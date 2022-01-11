0d140d0a19c5429b4405142c2ad7488e6eb76556
summathy <97403619+summathy@users.noreply.github.com>
2022-01-11 23:36:30 +0100

    Added navbar and content-body, still work in progress

--- backend/parser.js
+++ backend/parser.js
@@ -1,7 +1,10 @@
 function parseXML(path){
-	var contentString = "Ups, something went wrong :(";
+	var contentString = "";
 
 	/* read file */
+	var location = window.location.href;
+	var dirpath = location.substring(0,location.lastIndexOf("/")+1);
+	
 	try{
 		var xmlhttp = new XMLHttpRequest();
 		xmlhttp.open("GET", path, false);
@@ -10,10 +13,15 @@ function parseXML(path){
 			contentString = xmlhttp.responseText;
 		}
 	}catch(e){
-
+		contentString = "<h2>Section</h2>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.";
+		alert("Ups, something went wrong :(");
 	}
 
 	/* parse XML */
+
+	// TODO read and change title
+	// TODO add paragraphs
+	// TODO parse diagrams
 
 	/* write content */
 	var contentDiv = document.getElementById("content");
