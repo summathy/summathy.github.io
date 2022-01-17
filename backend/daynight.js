da76e1651e02944db251a1afbda0546e1cd8ff91
summathy <97403619+summathy@users.noreply.github.com>
2022-01-18 00:25:02 +0100

    Fixed Light- and Dark-Mode

--- backend/daynight.js
+++ backend/daynight.js
@@ -2,15 +2,15 @@ function toggleDayNight(){
 	const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)');
 	if (prefersDarkScheme.matches) {
 		document.body.classList.toggle('light-theme');
-		//document.getElementById('content').classList.toggle('dark-theme');
-		/*for(let element of document.getElementsByClassName('post')){
+		for(let element of document.getElementsByClassName('post')){
 			element.classlist.toggle('light-theme');
-		}*/
+		}
+		// TODO sections
 	} else {
 		document.body.classList.toggle('dark-theme');
-		//document.getElementById('content').classList.toggle('dark-theme');
-		/*for(let element of document.getElementsByClassName('post')){
-			element.toggle('dark-theme');
-		}*/
+		for(let element of document.getElementsByClassName('post')){
+			element.classList.toggle('dark-theme');
+		}
+		// TODO sections
 	}
 }
