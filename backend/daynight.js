d491fe06d54222b3f020aaf35fe6a4184683e772
summathy <97403619+summathy@users.noreply.github.com>
2022-01-20 00:08:41 +0100

    Worked on Navbar

--- backend/daynight.js
+++ backend/daynight.js
@@ -2,12 +2,14 @@ function toggleDayNight(){
 	const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)');
 	if (prefersDarkScheme.matches) {
 		document.body.classList.toggle('light-theme');
+		document.getElementById('navigation').classList.toggle('light-theme');
 		for(let element of document.getElementsByClassName('post')){
 			element.classList.toggle('light-theme');
 		}
 		// TODO sections
 	} else {
 		document.body.classList.toggle('dark-theme');
+		document.getElementById('navigation').classList.toggle('dark-theme');
 		for(let element of document.getElementsByClassName('post')){
 			element.classList.toggle('dark-theme');
 		}
